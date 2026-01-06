d4f96c050713b4309d5b1ada3a1f0ba6b0a50c14
Mohamed Reda <medorakha0o0@gmail.com>
2026-01-06 11:39:02 +0200

    lastt

--- js/examh.js
+++ js/examh.js
@@ -1,5 +1,18 @@
 import * as ls from "./LocalStorageManager.js";
 
+let permission_denied_err = sessionStorage.getItem("permission_denied");
+if(permission_denied_err)
+{
+  Swal.fire({title: "Error", text: "you dont have permission to do this action", icon: "error"});
+  sessionStorage.removeItem("permission_denied")
+}
+let user_type = ls.getStringKey("user_type");
+if(user_type != "student")
+{
+  sessionStorage.setItem("permission_denied", true);
+  window.history.back();
+}
+
     let finish=JSON.parse(sessionStorage.getItem("finished"));
 class StudentExams{
 
